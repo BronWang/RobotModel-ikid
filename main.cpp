d177e6fde169be1c2889c2cf1fcd6603c8056110
wp <1027706775@qq.com>
2022-10-06 10:57:07 +0800

    添加项目文件。

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+#include "robotModel.h"
+
+extern robotLink robotModel[26];
+extern double theta;
+int main(void) {	
+	robotStart();
+	double com[3];
+	Calc_com(com);
+	printf("ÖØÐÄ £º%f %f %f", com[0], com[1], com[2]);
+	trajPlan();
+	trajPlan();
+	//anglePlan(PI / 18);
+	trajPlan();
+	trajPlan();
+}
