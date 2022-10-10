a601aa1577ac78a8421dc35b4778c12ec2728a55
wp <1027706775@qq.com>
2022-10-10 11:24:25 +0800

    把README.md文件加到了工程中

--- main.cpp
+++ main.cpp
@@ -5,20 +5,14 @@
 
 extern robotLink robotModel[26];
 extern double theta;
-extern double sx;
-extern double sy;
 int main(void) {	
 	robotStart();
 	double com[3];
 	Calc_com(com);
 	printf("ÖØÐÄ £º%f %f %f", com[0], com[1], com[2]);
 	trajPlan();
-	anglePlan(PI / 6);
 	trajPlan();
-	anglePlan(PI / 6);
-	trajPlan();
-	trajPlan();
-	trajPlan();
+	//anglePlan(PI / 18);
 	trajPlan();
 	trajPlan();
 }
